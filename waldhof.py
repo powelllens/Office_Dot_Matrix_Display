3300d03d8f1311e74116258de2f4cfa7eada9071
David <progamer150@users.noreply.github.com>
2018-06-05 09:19:49 +0200

    Add files via upload

--- waldhof.py
+++ waldhof.py
@@ -1,41 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 
-import urllib2
-import shutil
+from bs4 import BeautifulSoup
 
 class waldhof:
     def getNewFootballTable(self):
-        self.url = 'file:./offline_page/svw07_home.htm'
-        self.f = urllib2.urlopen(self.url)
+        #try:
+        #  execfile('./ExtractWeb.py')
+        #except:
+        #  pass
         self.table_data = [[[] for _ in range(4)] for _ in range(5)]
+        soccertable_path = '/programs/office_display/soccer/soccertable.xml'
+        soccertable_path = './soccer/soccertable.xml'
+        
+        with open(soccertable_path) as f:
+            self.content = f.read()
+        f.close()
 
-        i = 0
-        self.table_point = 0
-        while i < 2000:
-            self.line = self.f.readline()
-            if '<TD class="views-field views-field-counter">' in self.line:
-                self.table_data[self.table_point][0] = self.line[(self.line.find('counter">')+9):-(len(self.line)-self.line.find('</TD>'))]
-                self.line = self.f.readline()
-                self.line = self.f.readline()
-                self.line = self.f.readline()
-                self.image = self.line[(self.line.find('src">')+19):-(len(self.line)-self.line.find('</TD>')+2)]
-                self.line = self.f.readline()
-                self.table_data[self.table_point][1] = self.line[(self.line.find('title">')+7):-(len(self.line)-self.line.find('</TD>'))]
-                self.line = self.f.readline()
-                self.table_data[self.table_point][2] = self.line[(self.line.find('difference">')+12):-(len(self.line)-self.line.find('</TD>'))]
-                self.line = self.f.readline()
-                self.table_data[self.table_point][3] = self.line[(self.line.find('points">')+8):-(len(self.line)-self.line.find('</TD>'))]
+        self.soccertable_xml = BeautifulSoup(self.content, 'html.parser')
+        self.soccertable = self.soccertable_xml.find('soccertable')
+        self.soccerteam = self.soccertable.find('soccer')
 
-                self.imagelocation = './offline_page/' + self.image
-                self.image_name = './images/' + self.table_data[self.table_point][0] + '_soccer.png'
-                shutil.copy(self.imagelocation, self.image_name)
-                #image = urllib2.URLopener()
-                #image.retrieve(self.url,self.image_name)
-
-                self.table_point = self.table_point + 1
-            if self.table_point > 4:
-                break
-            i = i + 1
-        self.f.close()
+        for self.soccerteam_counter in range(0,5):
+            self.place = self.soccerteam.find('place')
+            self.table_data[self.soccerteam_counter][0] = self.place.contents[0]
+            self.name = self.soccerteam.find('name')
+            self.table_data[self.soccerteam_counter][1] = self.name.contents[0]
+            self.point_difference = self.soccerteam.find('difference')
+            self.table_data[self.soccerteam_counter][2] = self.point_difference.contents[0]
+            self.points = self.soccerteam.find('points')
+            self.table_data[self.soccerteam_counter][3] = self.points.contents[0]
+            self.soccerteam = self.soccerteam.find_next('soccer')
+        
         return self.table_data
